55aac5756d71824053df1328bb19725d78b04a4c
Glenn K. Lockwood <glock@lbl.gov>
2019-05-06 15:05:32 -0700

    minor refactoring and cleanup

--- abcutils/sc18paper.py
+++ abcutils/sc18paper.py
@@ -50,52 +50,6 @@ def load_raw_datasets(input_datasets=None, cache_file=CACHE_FILE, verbose=True):
 
     return dataframe
 
-def load_dataset(verbose=True, truncate_contention=False, drop_cf_above=1.2, *args, **kwargs):
-    """Load dataset used for Year in the Life paper
-
-    Load the canonical dataset used for the "Year in the Life" paper, apply
-    global filters on the dataset, and add a few additional derived metrics.
-
-    Args:
-        verbose (bool): Print messages describing from where data is being
-            loaded
-        truncate_contention (bool): If True, apply max(0.0, val) to all
-            derived contention values.  Default value corresponds to what
-            was used in the paper.
-        drop_cf_above (float or None): Drop any records whose coverage factors
-            for bandwidth are above this value.  Default value corresponds to
-            what was used in the paper.
-
-    Returns:
-        pandas.DataFrame: Loaded, filtered, and augmented dataset
-    """
-    dataframe = load_raw_datasets(verbose=verbose, *args, **kwargs)
-
-    # Reset the index to ensure that there are no degenerate indices in the final dataframe
-    dataframe.index = pandas.Index(data=numpy.arange(len(dataframe)), dtype='int64')
-
-    # Apply a filter to invalidate obviously bogus bandwidth coverage factors
-    if drop_cf_above is not None:
-        for index in dataframe[dataframe['coverage_factor_bw'] > drop_cf_above].index:
-            dataframe.loc[index, 'coverage_factor_bw'] = numpy.nan
-
-
-    # Drop some of the weird columns left over from the CSV
-    dataframe = dataframe.drop(
-        columns=[x for x in ['Unnamed: 0', 'index'] if x in dataframe.columns],
-        axis=1)
-
-    filters = build_sc18_filters(dataframe)
-    filtered_df = abcutils.core.apply_filters(dataframe, filters, verbose).sort_values('_datetime_start').copy()
-
-    # Reset the index to ensure that there are no degenerate indices in the final dataframe
-    filtered_df.index = pandas.Index(data=numpy.arange(len(filtered_df)), dtype='int64')
-
-    del dataframe
-
-    return filtered_df
-
-
 def build_sc18_filters(dataframe):
     """Build generic data filters for the SC paper
 
@@ -125,3 +79,52 @@ def build_sc18_filters(dataframe):
     filters.append(dataframe['_test_platform'] != 'cscratch@cori-haswell')
 
     return filters
+
+def load_dataset(verbose=True, truncate_contention=False, drop_cf_above=1.2, filter_func=build_sc18_filters, *args, **kwargs):
+    """Load dataset used for Year in the Life paper
+
+    Load the canonical dataset used for the "Year in the Life" paper, apply
+    global filters on the dataset, and add a few additional derived metrics.
+
+    Args:
+        verbose (bool): Print messages describing from where data is being
+            loaded
+        truncate_contention (bool): If True, apply max(0.0, val) to all
+            derived contention values.  Default value corresponds to what
+            was used in the paper.
+        drop_cf_above (float or None): Drop any records whose coverage factors
+            for bandwidth are above this value.  Default value corresponds to
+            what was used in the paper.
+        filter_func: Function that takes a dataframe as an argument and returns
+            a list of filters that can be passed to
+            ``abcutils.core.apply_filters()``
+
+    Returns:
+        pandas.DataFrame: Loaded, filtered, and augmented dataset
+    """
+    dataframe = load_raw_datasets(verbose=verbose, *args, **kwargs)
+
+    # Reset the index to ensure that there are no degenerate indices in the final dataframe
+    dataframe.index = pandas.Index(data=numpy.arange(len(dataframe)), dtype='int64')
+
+    # Apply a filter to invalidate obviously bogus bandwidth coverage factors
+    if drop_cf_above is not None:
+        for index in dataframe[dataframe['coverage_factor_bw'] > drop_cf_above].index:
+            dataframe.loc[index, 'coverage_factor_bw'] = numpy.nan
+
+    # Drop some of the weird columns left over from the CSV
+    dataframe = dataframe.drop(
+        columns=[x for x in ['Unnamed: 0', 'index'] if x in dataframe.columns],
+        axis=1)
+
+    if filter_func:
+        filtered_df = abcutils.core.apply_filters(dataframe, filter_func(dataframe), verbose).sort_values('_datetime_start').copy()
+    else:
+        filtered_df = dataframe.sort_values('_datetime_start').copy()
+
+    # Reset the index to ensure that there are no degenerate indices in the final dataframe
+    filtered_df.index = pandas.Index(data=numpy.arange(len(filtered_df)), dtype='int64')
+
+    del dataframe
+
+    return filtered_df
